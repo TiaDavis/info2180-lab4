68429adabb7166f42a2280ed8f3b233434ac27fc
Tia Davis <tiajhaedavis258@gmail.com>
2021-10-30 23:39:20 -0400

    Implemented button

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+window.onload = function(){
+    let searchBtn = document.querySelector("#search-btn")
 
+    searchBtn.addEventListener("click", () => {
+        fetch("http://localhost/info2180-lab4/superheroes.php?q=")
+            .then(response => {
+                if(response.ok){
+                    return response.text()
+                }
+                else{
+                    throw new Error("An error has occured")
+                }
+            })
+            .then(data => {
+                alert(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+}
